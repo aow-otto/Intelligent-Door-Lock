36ac8b987408f51b434197f22766370fff856735
f-u-wangao <wangao0824@icloud.com>
2020-06-07 22:54:40 +0800

    默认更改列表

--- app/src/main/java/com/example/intelligentdoorlock/FaceControlActivity.java
+++ app/src/main/java/com/example/intelligentdoorlock/FaceControlActivity.java
@@ -1,6 +1,10 @@
 package com.example.intelligentdoorlock;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +18,29 @@ public class FaceControlActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_face_control);
         setSupportActionBar(toolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_face_control, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.item1:
+
+                break;
+            case R.id.item2:
+
+                break;
+            default:
+        }
+        return true;
     }
 }
